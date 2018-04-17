dc204cc8a75d9eca501c57b8bb6232e24a10d756
seinomasaki <omamori1221@gmail.com>
2018-04-17 12:53:42 +0900

    Add files via upload

--- poker-ruby/poker/hand.rb
+++ poker-ruby/poker/hand.rb
@@ -2,52 +2,52 @@ class Hand
   attr_reader :hand
 
   def initialize(hand)
-    @has_suit = hand[0]
+    @suit = hand[0]
     same_rank_lists = hand[1]
-    @number_of_the_same_rank = same_rank_lists[0]
-    @has_continuation = hand[2]
+    @count_of_the_same_rank = same_rank_lists[0]
+    @serial_number = hand[2]
   end
 
   def judg_score_card
-    if @has_continuation
+    if @serial_number
       serial_card
-    elsif !@has_suit && @number_of_the_same_rank.size == 1
+    elsif !@suit && @count_of_the_same_rank.size == 1
       one_set_of_same_rank
-    elsif !@has_suit && @number_of_the_same_rank.size == 2
+    elsif !@suit && @count_of_the_same_rank.size == 2
       two_sets_of_same_rank
-    elsif @has_suit && @number_of_the_same_rank.size == 0 && !@has_continuation
+    elsif @suit && @count_of_the_same_rank.size == 0 && !@serial_number
       "flush"
-    elsif !@has_suit && @number_of_the_same_rank.size == 0 && !@has_continuation
+    elsif !@suit && @count_of_the_same_rank.size == 0 && !@serial_number
       "high card"
     end
   end
 
   def one_set_of_same_rank
-    if @number_of_the_same_rank[0] == 4
+    if @count_of_the_same_rank[0] == 4
       "four of a kind"
-    elsif @number_of_the_same_rank[0] == 3
+    elsif @count_of_the_same_rank[0] == 3
       "three of a kind"
-    elsif @number_of_the_same_rank[0] == 2
+    elsif @count_of_the_same_rank[0] == 2
       "one pair"
     end
   end
 
   def two_sets_of_same_rank
-    if @number_of_the_same_rank.inject{|sum, i| sum + i} == 4
+    if @count_of_the_same_rank.inject{|sum, i| sum + i} == 4
       "two pair"
-    elsif @number_of_the_same_rank.inject{|sum, i| sum + i} == 5
+    elsif @count_of_the_same_rank.inject{|sum, i| sum + i} == 5
       "full house"
     end
   end
 
   def serial_card
-    if @has_suit
-      if @has_continuation.inject{|sum, i| sum + i} == 60
+    if @suit
+      if @serial_number.inject{|sum, i| sum + i} == 60
         "royal flush"
       else
         "straight flush"
       end
-    elsif !@has_suit
+    elsif !@suit
       "straight"
     end
   end
